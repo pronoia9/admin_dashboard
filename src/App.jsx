7a592e2b3ecbb412ac4b838761029509f6221bc1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-10 21:10:06 -0400

    add app styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <div>App</div>;
+};
 
-export default App
+export default App;
