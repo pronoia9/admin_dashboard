f74ada48cdc3fad8ade3a782125df632b8f82abc
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-18 16:28:09 -0400

    move style classes to sidebar

--- src/App.js
+++ src/App.js
@@ -22,17 +22,14 @@ function App() {
               </button>
             </TooltipComponent>
           </div>
-
-          {/* Sidebar */}
-          {activeMenu ? (
-            <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'><Sidebar /></div>
-          ) : (
-            <div className='w-0 dark:bg-secondary-dark-bg'><Sidebar /></div>
-          )}
+          
+          <Sidebar />
 
           {/* Navbar */}
           <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
-            <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'><Navbar /></div>
+            <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+              <Navbar />
+            </div>
           </div>
 
           <div>
