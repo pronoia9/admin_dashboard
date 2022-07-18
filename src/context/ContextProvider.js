bee1c1402a50991ae94e3b74d3d5f52abe5353c7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-18 18:38:08 -0400

    add setactivemenu to statecontext prov

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -12,7 +12,7 @@ const initialState = {
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
 
-  return <StateContext.Provider value={{ activeMenu, }}>{children}</StateContext.Provider>;
+  return <StateContext.Provider value={{ activeMenu, setActiveMenu }}>{children}</StateContext.Provider>;
 };
 
 export const useStateContext = () => useContext(StateContext);
