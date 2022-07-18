bb0766d9fa3b967f15df161f4adf71b87b3da1a6
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-18 16:28:25 -0400

    add activemenu check and style classes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+const Sidebar = ({ activeMenu }) => {
+  return <div className={`dark:bg-secondary-dark-bg ${activeMenu ? 'w-72 fixed sidebar bg-white' : 'w-0'}`}></div>;
 };
 
 export default Sidebar;
