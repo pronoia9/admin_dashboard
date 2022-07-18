f6451e2ccc2d94f8176c5b7f1dc00770dac444fc
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-18 18:39:01 -0400

    format, remove activemenu

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components'
-import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line } from './pages'
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line } from './pages';
+
 import './App.css';
 
 function App() {
-  const activeMenu = true;
   return (
     <div>
       <BrowserRouter>
