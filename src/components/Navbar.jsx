233716832163bab944da80eceb844241006b9adc
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-18 18:56:08 -0400

    add context

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useStateContext } from '../context/ContextProvider';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { activeMenu, setActiveMenu } = useStateContext();
+
+  return (
+    <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
+      <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>{/*  */}</div>
+    </div>
+  );
 };
 
 export default Navbar;
