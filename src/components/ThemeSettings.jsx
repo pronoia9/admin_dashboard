28a78fa054e7c2f3a877d4d29ac36f817cb0de54
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-23 11:28:38 +0300

    turn theme settings menu to false on close

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -7,6 +7,8 @@ import { themeColors } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const ThemeSettings = () => {
+  const { setColor, setMode, currentMode, currentColor, setThemeSettings } = useStateContext();
+
   return (
     <div id='theme_settings-component' className='bg-half-transparent w-screen fixed nav-item top-0 right-0'>
       <div className='float-right h-screen dark:text-gray-200  bg-white dark:bg-[#484B52] w-400'>
@@ -15,7 +17,7 @@ const ThemeSettings = () => {
           <p className='font-semibold text-lg'>Settings</p>
           <button
             type='button'
-            onClick={() => () => {}}
+            onClick={() => setThemeSettings(false)}
             style={{ color: 'rgb(153, 171, 180)', borderRadius: '50%' }}
             className='text-2xl p-3 hover:drop-shadow-xl hover:bg-light-gray'>
             <MdOutlineCancel />
