cbfca19253564d265884ccd90bb9e8d02f54b4a7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-23 11:50:33 +0300

    apply currentColor context + format

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -9,6 +9,8 @@ import { earningData, medicalproBranding, recentTransactions, weeklyStats, dropd
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Ecommerce = () => {
+  const { currentColor } = useStateContext();
+
   return (
     <div className='mt-24'>
       <div className='flex flex-wrap lg:flex-nowrap justify-center'>
@@ -21,7 +23,7 @@ const Ecommerce = () => {
             </div>
           </div>
           <div className='mt-6'>
-            <Button color='white' bgColor='blue' text='Download' borderRadius='10px' />
+            <Button color='white' bgColor={currentColor} text='Download' borderRadius='10px' />
           </div>
         </div>
 
@@ -29,19 +31,13 @@ const Ecommerce = () => {
         <div className='flex m-3 flex-wrap justify-center gap-1 items-center'>
           {earningData.map((item) => (
             <div key={item.title} className='bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl'>
-              <button
-                type='button'
-                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
-                className='text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl'>
+              <button type='button' style={{ color: item.iconColor, backgroundColor: item.iconBg }} className='text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl'>
                 {item.icon}
               </button>
               <p className='mt-3'>
-                {/* Number */}
                 <span className='text-lg font-semibold'>{item.amount}</span>
-                {/* Percentage (gain/loss) */}
                 <span className={`text-sm text-${item.pcColor} ml-2`}>{item.percentage}</span>
               </p>
-              {/* Card title */}
               <p className='text-sm text-gray-400 mt-1'>{item.title}</p>
             </div>
           ))}
@@ -56,15 +52,11 @@ const Ecommerce = () => {
             <p className='font-semibold text-xl'>Revenue Updates</p>
             <div className='flex items-center gap-4'>
               <p className='flex items-center gap-2 text-gray-600 hover:drop-shadow-xl'>
-                <span>
-                  <GoPrimitiveDot />
-                </span>
+                <span><GoPrimitiveDot /></span>
                 <span>Expense</span>
               </p>
               <p className='flex items-center gap-2 text-green-400 hover:drop-shadow-xl'>
-                <span>
-                  <GoPrimitiveDot />
-                </span>
+                <span><GoPrimitiveDot /></span>
                 <span>Budget</span>
               </p>
             </div>
@@ -77,9 +69,7 @@ const Ecommerce = () => {
               <div>
                 <p>
                   <span className='text-3xl font-semibold'>$93,438</span>
-                  <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs'>
-                    23%
-                  </span>
+                  <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs'>23%</span>
                 </p>
                 <p className='text-gray-500 mt-1'>Budget</p>
               </div>
@@ -92,18 +82,10 @@ const Ecommerce = () => {
 
               {/* Revenue graph (sparkline) */}
               <div className='mt-5'>
-                <SparkLine
-                  currentColor='blue'
-                  id='line-sparkLine'
-                  type='Line'
-                  height='80px'
-                  width='250px'
-                  data={SparklineAreaData}
-                  color='green'
-                />
+                <SparkLine currentColor={currentColor} id='line-sparkLine' type='Line' height='80px' width='250px' data={SparklineAreaData} color={currentColor} />
               </div>
               <div className='mt-10'>
-                <Button color='white' bgColor='blue' text='Download Report' borderRadius='10px' />
+                <Button color='white' bgColor={currentColor} text='Download Report' borderRadius='10px' />
               </div>
             </div>
 
