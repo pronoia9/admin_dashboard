5a9957ba4205d5473c3256783105969fa2351f18
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-23 22:57:42 +0300

    remove console log

--- src/pages/Charts/ColorMapping.jsx
+++ src/pages/Charts/ColorMapping.jsx
@@ -7,7 +7,6 @@ import { useStateContext } from '../../contexts/ContextProvider';
 
 const ColorMapping = () => {
   const { chartStyles } = useStateContext();
-  console.log({ ...chartStyles.axisStyles });
 
   return (
     <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
