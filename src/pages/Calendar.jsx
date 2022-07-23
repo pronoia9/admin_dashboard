118de59b1a30c156c1c495a7bd4b9ef83c423661
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-23 13:12:30 +0300

    add custom color to calendar

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -3,12 +3,19 @@ import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, 
 
 import { scheduleData } from '../data/dummy';
 import { Header } from '../components';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Scheduler = () => {
+  const { currentColor } = useStateContext();
+
   return (
     <div id='calendar-page' className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
       <Header category='App' title='Calendar' />
-      <ScheduleComponent height='650px' selectedDate={new Date(2021, 0, 10)} eventSettings={{ dataSource: scheduleData }}>
+      <ScheduleComponent
+        height='650px'
+        selectedDate={new Date(2021, 0, 10)}
+        eventSettings={{ dataSource: scheduleData }}
+        cssClass={`custom-class-${currentColor.replace('#', '')}`}>
         <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]} />
       </ScheduleComponent>
     </div>
