fd597c9c7007cfde71f036173c7aeb9de335d130
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-23 11:23:28 +0300

    add theme + color state and funcs

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -13,13 +13,25 @@ export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
   const [isClicked, setIsClicked] = useState(initialState);
   const [screenSize, setScreenSize] = useState(undefined);
+  const [currentColor, setCurrentColor] = useState('#03C9D7');
+  const [currentMode, setCurrentMode] = useState('Light');
+
+  const setColor = (e) => {
+    setCurrentColor(e.target.value);
+    localStorage.setItem('color Mode', e.target.value);
+  };
+
+  const setMode = (e) => {
+    setCurrentMode(e.target.value);
+    localStorage.setItem('themeMode', e.target.value);
+  }
 
   const handleClick = (clicked) => {
     setIsClicked({ ...initialState, [clicked]: true });
   };
 
   return (
-    <StateContext.Provider value={{ activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize }}>
+    <StateContext.Provider value={{ activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize, currentColor, setCurrentColor, setColor, currentMode, setCurrentMode, setMode }}>
       {children}
     </StateContext.Provider>
   );
